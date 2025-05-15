254b21b424176531e7e2eb95eb0bd3434e5045d9
Lee Nevison <leenevison516@gmail.com>
2025-05-15 22:39:12 +0800

    Update product.route.js
    Removed getProducts and added getAllProducts

--- routers/product.route.js
+++ routers/product.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const Product = require("../models/product.model.js");
 const router = express.Router();
-const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller.js');
+const {getProduct, getAllProducts, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller.js');
 
-router.get('/', getProducts);
+router.get('/all', getAllProducts);
+
 router.get("/:id", getProduct);
 
 router.post("/",createProduct);
