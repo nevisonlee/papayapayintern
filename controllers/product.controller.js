454ca807e74c22a1879904ac186206598ad2f03b
Lee Nevison <leenevison516@gmail.com>
2025-05-15 22:38:17 +0800

    Update product.controller.js
    Removed getProducts and added getAllProducts

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,19 +1,5 @@
 const Product = require('../models/product.model');
 
-
-const getProducts = async (req, res) => {
-
-        try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-
-}
-
 const getProduct = async (req, res) => {
     try {
         const { id } = req.params;
@@ -24,6 +10,15 @@ const getProduct = async (req, res) => {
         res.status(500).json({message: error.message});
     }
 }
+
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 const createProduct = async (req, res) => {
     try {
@@ -71,9 +66,10 @@ const deleteProduct = async (req, res) => {
 
 
 module.exports = {
-    getProducts,
     getProduct,
+    getAllProducts,
     createProduct,
     updateProduct,
     deleteProduct
 }
+
